c3a1d22ac0741559d382c8d0d8feb18b872a870f
bartoszgebarowski <gebarowski.bartosz@gmail.com>
2022-10-17 20:26:39 +0200

    implement allauth

--- lorecraft/urls.py
+++ lorecraft/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from static_pages.views import HomeView
+from django.urls import include, path
+
 from accounts import views
+from static_pages.views import HomeView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', HomeView.as_view()),
-    path('accounts/', include('accounts.urls'))
+    path("", HomeView.as_view()),
+    path("accounts/", include("allauth.urls")),
 ]
