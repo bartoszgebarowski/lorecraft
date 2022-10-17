7fac6fe77d67deaf2d39fb1d5b0a77d53e9c91b4
bartoszgebarowski <gebarowski.bartosz@gmail.com>
2022-10-17 15:30:58 +0200

    add accounts urls and setup AUTH_USER_MODEL

--- lorecraft/settings.py
+++ lorecraft/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "cloudinary",
     "static_pages",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -144,3 +145,5 @@ DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'accounts.MyUser'
--- lorecraft/urls.py
+++ lorecraft/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from static_pages.views import HomeView
+from accounts import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', HomeView.as_view())
+    path('', HomeView.as_view()),
+    path('accounts/', include('accounts.urls'))
 ]
